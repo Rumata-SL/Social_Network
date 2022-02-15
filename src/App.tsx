586a8b2d4e7a484ff0f73a60878e3916ecd6b783
Rumata-SL <kirr7619@gmail.com>
2022-02-16 02:05:04 +0500

    Added directory "dialogs", and files dialogs.tsx, dialogs.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import {Header} from "./components/header/heder";
 import {Navbar} from "./components/navbar/navbar";
 import {Profile} from "./components/profile/profile";
+import {Dialogs} from "./components/dialogs/dialogs";
 
 function App() {
     return (
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            <Profile/>
+            {/*<Profile/>*/}
+            <Dialogs/>
         </div>
     );
 }
